35eddfef345885bc73e9870c1e0c0666d8d7a685
alchemis <hopefuldecay@gmail.com>
2026-08-05 04:54:25 -0300

    added articuno crest

---
+++ patch/Mods/LyraVickyCrests/ArticunoCrest.rb
@@ -0,0 +1,43 @@
+PBStuff::POKEMONTOCREST[:ARTICUNO] = :ARTICREST
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:ARTICREST] = ItemData.new(:ARTICREST, {
+    name: "Articuno Crest",
+    desc: "Articuno causes it to snow for 8 turns on switch-in, its ice-type weaknesses are negated during snow.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+class PokeBattle_Move
+  alias articrest_irregularTypeMods irregularTypeMods
+  def irregularTypeMods(attacker, opponent, typemod, type)
+    typemod = articrest_irregularTypeMods(attacker, opponent, typemod, type)
+    case opponent.crested
+      when :ARTICUNO
+        if @battle.pbWeather(attacker) == :SNOW || @battle.pbWeather(attacker) == :HAIL  then
+          typemod *= Typemod.half if [:FIGHTING, :ROCK, :FIRE, :STEEL].include?(type)
+        end
+    end
+    return typemod
+  end
+end
+
+class PokeBattle_Battle
+    alias articrest_pbCrestEntry pbCrestEntry
+    def pbCrestEntry(index, pokemon)
+      articrest_pbCrestEntry(index, pokemon)
+      battler = @battlers[index]
+      case battler.crested
+        when :ARTICUNO
+          return if !canSetWeather?(:SNOW)
+          duration = [:ICY, :SNOWYMOUNTAIN, :FROZENDIMENSION, :SKY, :CLOUDS].include?(@field.effect) ? 11 : 8
+          pbShowAbilityBox(battler, item: true)
+          pbSetWeather(:SNOW, duration)
+          pbHideAbilityBox(battler)
+      end
+    end
+end
