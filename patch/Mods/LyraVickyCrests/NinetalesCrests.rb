c07e764192d98cf09cb217f03f7f9c93261738e6
alchemis <hopefuldecay@gmail.com>
2026-08-15 19:34:23 -0300

    raichu and ninetales (both)

---
+++ patch/Mods/LyraVickyCrests/NinetalesCrests.rb
@@ -0,0 +1,104 @@
+#PBStuff::POKEMONTOCREST[:NINETALES] = :LVCATALESCREST
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:LVCATALESCREST] = ItemData.new(:LVCATALESCREST, {
+    name: "A. Ninetales Crest",
+    desc: "Counts as having weather rocks and Light Clay. Boosts Special Attack.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:LVCNINETALESCREST] = ItemData.new(:LVCNINETALESCREST, {
+    name: "K. Ninetales Crest",
+    desc: "Gains Ghost-type STAB and resistances. Curses if hit by a contact move. Boosts Special Attack.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+class PokeBattle_Battler
+    #form check..
+    alias atalescrest_hasCrest? hasCrest? if !defined?(atalescrest_hasCrest?)
+    def hasCrest?(species = self.species)
+        if species == :NINETALES then
+          return true if @item == :LVCATALESCREST && form == 1
+          return true if @item == :LVCNINETALESCREST && form == 0
+          return false
+        else return atalescrest_hasCrest?(species)
+        end
+    end
+
+
+    #stats
+    alias atalescrest_crestStats crestStats if !defined?(atalescrest_crestStats)
+    def crestStats
+      atalescrest_crestStats
+      case @crested
+        when :NINETALES
+            @spatk *= 1.2
+      end
+    end
+
+    #item stuff
+    alias atalescrest_hasWorkingItem hasWorkingItem if !defined?(atalescrest_hasWorkingItem)
+    def hasWorkingItem(item, ignorefainted: false)
+      ignorefainted = false if !defined?(ignorefainted)
+      if @crested == :NINETALES && form == 1 then
+        case item
+        when :ICYROCK then return true
+        when :HEATROCK then return true
+        when :DAMPROCK then return true
+        when :SMOOTHROCK then return true
+        when :LIGHTCLAY then return true
+        end
+      end
+      return atalescrest_hasWorkingItem(item, ignorefainted: ignorefainted)
+    end
+
+    #curse effect
+    alias atalescrest_pbEffectsOnDealingDamage pbEffectsOnDealingDamage if !defined?(atalescrest_pbEffectsOnDealingDamage)
+    def pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent = false)
+      if target.crested == :NINETALES && target.form == 0 then
+        if user.makesContact?(move) && !user.effects[:Curse] 
+          @battle.pbShowAbilityBox(target, item:true)
+          @battle.pbDisplay(_INTL("Touching {1} put a curse on {2}!", target.pbThis, user.pbThis(true)))
+          user.effects[:Curse] = true
+          @battle.pbHideAbilityBox(target)
+        end
+      end
+      return atalescrest_pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent || false)
+    end
+
+
+end
+
+class PokeBattle_Move
+
+  alias atalescrest_pbModifySTAB pbModifySTAB if !defined?(atalescrest_pbModifySTAB)
+  def pbModifySTAB(stabmult, type, attacker, opponent)
+    if attacker.crested == :NINETALES && attacker.form == 0 && type == :GHOST then
+        stabmult += 0.5 if stabmult <= 1
+    end
+    return atalescrest_pbModifySTAB(stabmult, type, attacker, opponent)
+  end
+
+  alias atalescrest_irregularTypeMods irregularTypeMods if !defined?(atalescrest_irregularTypeMods)
+  def irregularTypeMods(attacker, opponent, typemod, type)
+    typemod = atalescrest_irregularTypeMods(attacker, opponent, typemod, type)
+    if opponent.crested == :NINETALES && opponent.form == 0
+        typemod *= Typemod.half if [:POISON, :BUG].include?(type) 
+        typemod *= Typemod.zero if [:NORMAL, :FIGHTING].include?(type) 
+    end
+    return typemod
+  end
+
+
+end
---
+++ patch/Mods/LyraVickyCrests/RaichuCrest.rb
@@ -0,0 +1,39 @@
+PBStuff::POKEMONTOCREST[:RAICHU] = :LVCRAICHUCREST
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:LVCRAICHUCREST] = ItemData.new(:LVCRAICHUCREST, {
+    name: "Raichu Crest",
+    desc: "Raichu uses Ion Deluge on switch-in, also increases attacking stats by 30%.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+
+
+class PokeBattle_Battle
+    alias raichucrest_pbCrestEntry pbCrestEntry if !defined?(raichucrest_pbCrestEntry)
+    def pbCrestEntry(index, pokemon)
+      raichucrest_pbCrestEntry(index, pokemon)
+      battler = @battlers[index]
+      case battler.crested
+        when :RAICHU
+          battler.pbUseMoveSimple(:IONDELUGE, -1, -1)
+      end
+    end
+end
+
+class PokeBattle_Battler
+    alias raichucrest_crestStats crestStats if !defined?(raichucrest_crestStats)
+    def crestStats
+      raichucrest_crestStats
+      case @crested
+        when :RAICHU
+            @attack *= 1.3
+            @spatk *= 1.3
+      end
+    end
+end
