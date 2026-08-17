baf0366c9f185d4b5f4ebf0a48fa006575f09cb2
alchemis <hopefuldecay@gmail.com>
2026-08-17 03:16:18 -0300

    gen v fossil crests

---
+++ patch/Mods/LyraVickyCrests/ArcheopsCrest.rb
@@ -0,0 +1,86 @@
+
+PBStuff::POKEMONTOCREST[:ARCHEOPS] = :LVCARCHCREST
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:LVCARCHCREST] = ItemData.new(:LVCARCHCREST, {
+    name: "Archeops Crest",
+    desc: "If at full hp, can't take over half its health in damage. Switches out when at half health or less and regenerates.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+
+
+#cant take more than half its hp if at full
+class PokeBattle_Move
+    alias archcrest_pbCalcDamage pbCalcDamage if !defined?(archcrest_pbCalcDamage)
+    def pbCalcDamage(attacker, opponent, hitnum = 0, feedbackMessages = { opponent.index => [] }, movetype: nil)
+      if movetype then
+        damage = archcrest_pbCalcDamage(attacker, opponent, hitnum, feedbackMessages, movetype: movetype) #attacker, opponent, hitnum, feedbackMessages, movetype
+      else damage = archcrest_pbCalcDamage(attacker, opponent, hitnum, feedbackMessages)
+      end
+      if opponent.crested == :ARCHEOPS and opponent.hp == opponent.totalhp and damage >= (opponent.totalhp/2)
+        opponent.effects[:LVC_TANKEDHIT] = true
+        damage = [damage, (opponent.totalhp/2.to_f).ceil].min()
+
+      end
+      return damage
+      
+    end
+end
+
+
+class PokeBattle_Battler
+  #regen
+  alias archcrest_pbInitialize pbInitialize if !defined?(archcrest_pbInitialize)
+  def pbInitialize(pkmn, index, batonpass)
+    ret = archcrest_pbInitialize(pkmn, index, batonpass)
+    if self.crested == :ARCHEOPS && @pokemon && @hp > 0 && self.status != :PETRIFIED
+      self.pbRecoverHP((self.totalhp / 3.0).floor, false, false) # Healing isn't shown on HP bar before switching
+    end
+    return ret
+  end
+  #e. exit
+  alias archcrest_pbEmergencyExitCheck pbEmergencyExitCheck if !defined?(archcrest_pbEmergencyExitCheck)
+  def pbEmergencyExitCheck(oldhp)
+    if self.crested == :ARCHEOPS then
+    return unless oldhp > (@totalhp / 2.0).floor && self.hp <= (@totalhp / 2.0).floor && self.hp != 0
+
+    if @battle.FE == :COLOSSEUM
+        @battle.pbAbilityBoxAndDisplay(self, _INTL("{1} has nowhere to run!", self.pbThis), item: true)
+    else
+      if @battle.pbIsWild? and @battle.pbIsOpposing?(self.index)
+        # when a boss is initialized cantescape is getting set on the battle, so wild bosses will never be fleed from unless the boss's settings explicitly allow it
+        unless @battle.cantescape || $game_switches[:Never_Escape]
+          @battle.pbShowAbilityBox(self, item:true)
+          pbSEPlay("escape", 100)
+          @battle.pbDisplayPaused(_INTL("{1} fled!", self.pbThis))
+          @battle.pbHideAbilityBox(self)
+          @battle.decision = 3 # Set decision to escaped
+        end
+      else
+        if self.canTriggerUserSwitch?
+          message = _INTL("{1} slinks away in defeat!", self.pbThis)
+          @battle.pbAbilityBoxAndDisplay(self, message, item: true)
+          self.userSwitch = :Ability
+        end
+      end
+    end
+    else return archcrest_pbEmergencyExitCheck(oldhp)    
+    end 
+  end
+  #just for abilitybox
+  alias archcrest_pbEffectsOnDealingDamage pbEffectsOnDealingDamage if !defined?(archcrest_pbEffectsOnDealingDamage)
+  def pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent = false)
+    if target.effects[:LVC_TANKEDHIT] && target.crested == :ARCHEOPS
+      @battle.pbAbilityBoxAndDisplay(target, _INTL("{1} tanked the hit!", target.pbThis), item: true)
+      target.effects[:LVC_TANKEDHIT] = false
+      @battle.pbHideAbilityBox(target)
+    end
+    return archcrest_pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent || false)
+  end
+end
---
+++ patch/Mods/LyraVickyCrests/CarracostaCrest.rb
@@ -0,0 +1,65 @@
+
+PBStuff::POKEMONTOCREST[:CARRACOSTA] = :LVCCARRACREST
+
+
+ModCacheInjection.hook(:items) {
+  $cache.items[:LVCCARRACREST] = ItemData.new(:LVCCARRACREST, {
+    name: "Carracosta Crest",
+    desc: "If at full hp, can't take over half its health in damage. Sharply boosts defenses at half health or less.",
+    price: 0,
+    crest: true,
+    noUseInBattle: true,
+    noUse: true,
+  })
+}
+
+
+
+#cant take more than half its hp if at full
+class PokeBattle_Move
+    alias carracrest_pbCalcDamage pbCalcDamage if !defined?(carracrest_pbCalcDamage)
+    def pbCalcDamage(attacker, opponent, hitnum = 0, feedbackMessages = { opponent.index => [] }, movetype: nil)
+      if movetype then
+        damage = carracrest_pbCalcDamage(attacker, opponent, hitnum, feedbackMessages, movetype: movetype) #attacker, opponent, hitnum, feedbackMessages, movetype
+      else damage = carracrest_pbCalcDamage(attacker, opponent, hitnum, feedbackMessages)
+      end
+      if opponent.crested == :CARRACOSTA and opponent.hp == opponent.totalhp and damage >= (opponent.totalhp/2)
+        opponent.effects[:LVC_TANKEDHIT] = true
+        damage = [damage, (opponent.totalhp/2.to_f).ceil].min()
+
+      end
+      return damage
+      
+    end
+end
+
+
+class PokeBattle_Battler
+
+  #sharply raise defenses
+  alias carracrest_pbEmergencyExitCheck pbEmergencyExitCheck if !defined?(carracrest_pbEmergencyExitCheck)
+  def pbEmergencyExitCheck(oldhp)
+    if self.crested == :CARRACOSTA then
+    return unless oldhp > (@totalhp / 2.0).floor && self.hp <= (@totalhp / 2.0).floor && self.hp != 0
+
+    if self.pbCanIncreaseAnyStat?([PBStats::DEFENSE, PBStats::SPDEF], self, nil, showMessage: false)
+      @battle.pbShowAbilityBox(self, item: true)
+      @battle.pbDisplay(_INTL("{1} bunkers down!", self.pbThis))
+      self.pbChangeStats([PBStats::DEFENSE, PBStats::SPDEF], 2, self, nil, abilitycheck: :skip)
+      @battle.pbHideAbilityBox(self)
+    end
+    else return carracrest_pbEmergencyExitCheck(oldhp)    
+    end 
+  end
+
+  #just for abilitybox
+  alias carracrest_pbEffectsOnDealingDamage pbEffectsOnDealingDamage if !defined?(carracrest_pbEffectsOnDealingDamage)
+  def pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent = false)
+    if target.effects[:LVC_TANKEDHIT] && target.crested == :CARRACOSTA
+      @battle.pbAbilityBoxAndDisplay(target, _INTL("{1} tanked the hit!", target.pbThis), item: true)
+      target.effects[:LVC_TANKEDHIT] = false
+      @battle.pbHideAbilityBox(target)
+    end
+    return carracrest_pbEffectsOnDealingDamage(move, user, target, damage, attackerNotPresent || false)
+  end
+end
