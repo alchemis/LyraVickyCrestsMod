80802a5eb80c674fdb36eb428d449ce1b6206e1c
Victory4T <victoriawilliams4t@gmail.com>
2026-08-28 16:35:29 -0400

    Tropius Description v3

--- patch/Mods/LyraVickyCrests/TropiusCrest.rb
+++ patch/Mods/LyraVickyCrests/TropiusCrest.rb
@@ -4,7 +4,7 @@ PBStuff::POKEMONTOCREST[:TROPIUS] = :LVCTROPCREST
 ModCacheInjection.hook(:items) {
   $cache.items[:LVCTROPCREST] = ItemData.new(:LVCTROPCREST, {
     name: "Tropius Crest",
-    desc: "Tropis gains the Dragon-Type on entry, also its Offenses and Speed are boosted. In Sun, Ice-type moves become Water.",
+    desc: "Tropius gains the Dragon-Type on entry, also its offenses are boosted. In Sun, Ice-type moves become Water.",
     price: 0,
     crest: true,
     noUseInBattle: true,
