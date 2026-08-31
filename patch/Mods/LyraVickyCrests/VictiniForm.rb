dd606a48ad24b6c706f63cee669685613e6204db
Victory4T <victoriawilliams4t@gmail.com>
2026-08-31 00:40:44 -0400

    Victini Form
    Let it Kill People

---
+++ patch/Mods/LyraVickyCrests/VictiniForm.rb
@@ -0,0 +1,48 @@
+if !(File.exist?('patch/Init/0000.cache_injection.rb') or !File.exist?('patch/Init/0000.map_injection.rb')) then 
+  print("Error, patching libraries not found. Please download 0000.cache_injection.rb and 0000.map_injection.rb from wiresegal's modpack at: github.com/yrsegal/rejuvenation-modpack")
+end
+
+#Forms
+ModCacheInjection.hook(:pkmn) {
+
+  $cache.pkmn[:VICTINI][0,1].RelearnerMoves.push(:CELEBRATE, :FREEZESHOCK, :ICEBURN, :VICTORYDANCE, :VDEVASTATE)
+  ModCacheInjection.createNewForm(:VICTINI,"Promised Victory",1,
+      {
+        :Type1 => :FAIRY,
+        :Type2 => :FIRE,
+        :BaseStats => [110, 120, 80, 120, 80, 110],
+		    :Abilities => [:VICTORYSTAR],
+        :ExcludeDex => true,
+      }
+  )}
+
+#Move
+ModCacheInjection.hook(:moves) {
+  $cache.moves[:VDEVASTATE] = MoveData.new(:VDEVASTATE,{
+    :name => "V-devastate",
+    :desc => "Uses the light of a promised Victory to devastate the opponent. Uses the highest attacking stat.",
+    :function => 0x309, #SHELL SIDE ARM
+    :type => :FAIRY,
+    :category => :physical,
+    :basedamage => 140,
+    :accuracy => 85,
+    :maxpp => 16,
+    :target => :SingleNonUser,
+    :contact => false,
+    :dancemove => true
+  })
+
+}
+
+class PokeBattle_Move_309 < PokeBattle_Move #SHELL SIDE ARM
+
+  def pbShowAnimation(id,attacker,opponent,hitnum=0,alltargets=nil,showanimation=true)
+    if showanimation
+      if id == :VDEVASTATE
+        @battle.pbAnimation(:JUDGMENTFAIRY,attacker,opponent,hitnum)
+      else
+        @battle.pbAnimation(id,attacker,opponent,hitnum)
+      end
+    end
+  end
+end
